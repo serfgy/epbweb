39a8c9668fcb6463955fbe7631437a7cdc534488
serfgy <xulie_alex@hotmail.com>
2020-04-23 18:09:43 +0800

    contact pic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,6 +5,9 @@ import { AntDesign } from 'react-web-vector-icons';
 import Home6 from './Home6';
 import Header from './Header';
 import Footer from './Footer';
+import branch1 from './images/branch1.jpg';
+import branch2 from './images/branch2.jpg';
+import branch3 from './images/branch3.jpg';
 
 class Contact extends Component {
     constructor(props) {
@@ -24,18 +27,19 @@ class Contact extends Component {
                 <Header />
                 <div style={styles.contact0}>
                     <div className='body-col-2'>
-                        <div style={styles.title}>Contact Us</div>
-                        <div style={styles.subtitle}>Leave us a message and we will get to you shortly!</div>
-                        <div style={styles.inputTitle}>NAME<span style={{ color: 'red' }}>*</span></div>
-                        <input style={styles.input} />
-                        <div style={styles.inputTitle}>EMAIL<span style={{ color: 'red' }}>*</span></div>
-                        <input style={styles.input} />
-                        <div style={styles.inputTitle}>COMPANY</div>
-                        <input style={styles.input} />
-                        <div style={styles.inputTitle}>MESSAGE</div>
-                        <textarea style={styles.textarea} />
-                        <div style={styles.button}>GET IN TOUCH</div>
-
+                        <div style={styles.wrapper}>
+                            <div style={styles.title}>Contact Us</div>
+                            <div style={styles.subtitle}>Leave us a message and we will get to you shortly!</div>
+                            <div style={styles.inputTitle}>NAME<span style={{ color: 'red' }}>*</span></div>
+                            <input style={styles.input} />
+                            <div style={styles.inputTitle}>EMAIL<span style={{ color: 'red' }}>*</span></div>
+                            <input style={styles.input} />
+                            <div style={styles.inputTitle}>COMPANY</div>
+                            <input style={styles.input} />
+                            <div style={styles.inputTitle}>MESSAGE</div>
+                            <textarea style={styles.textarea} />
+                            <div style={styles.button}>GET IN TOUCH</div>
+                        </div>
                     </div>
                     <div className='body-col-2'>
                         <div style={styles.wrapper}>
@@ -45,12 +49,14 @@ class Contact extends Component {
                                 <div style={styles.branchAddress}>#07-65 Vertex Tower A</div>
                                 <div style={styles.branchAddress}>Singapore 408868</div>
                                 <div style={styles.branchPhone}>+65 6509 4065</div>
+                                <div style={styles.image1}></div>
                             </div>
                             <div style={styles.branchContainer}>
                                 <div style={styles.branchTitle}>Shanghai</div>
                                 <div style={styles.branchAddress}>Changlin Road, Baoshen District</div>
                                 <div style={styles.branchAddress}>Room 5014, No. 913</div>
                                 <div style={styles.branchPhone}>+ 86 21 6386 8208</div>
+                                <div style={styles.image2}></div>
                             </div>
                             <div style={styles.branchContainer}>
                                 <div style={styles.branchTitle}>Taipei</div>
@@ -58,13 +64,14 @@ class Contact extends Component {
                                 <div style={styles.branchAddress}>Room 1201, 12F, No. 333</div>
                                 <div style={styles.branchAddress}>Taipei 11012</div>
                                 <div style={styles.branchPhone}>+ 886 2 2361 9781</div>
+                                <div style={styles.image3}></div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <Home6 />
                 <Footer />
-            </div>
+            </div >
         )
     }
 }
@@ -85,22 +92,22 @@ const styles = ({
     title: {
         fontFamily: 'raleway-bold',
         fontSize: 16,
-        margin: '60px 20px 0 20px',
+        margin: '60px 0px 0 0px',
     },
     subtitle: {
         fontFamily: 'lato-regular',
         fontSize: 16,
-        margin: '0px 20px 60px 20px',
+        margin: '0px 0px 60px 0px',
     },
     inputTitle: {
         fontFamily: 'lato-regular',
         fontSize: 12,
-        margin: '0px 20px 0px 20px',
+        margin: '0px 0px 0px 0px',
     },
     input: {
-        width: '100%',
+        // width: '100%',
         maxWidth: 400,
-        margin: '10px 20px 40px 20px',
+        margin: '10px 0px 40px 0px',
         padding: '10px 10px 10px 10px',
         borderRadius: 5,
         border: 'none',
@@ -110,9 +117,9 @@ const styles = ({
         fontSize: 16,
     },
     textarea: {
-        width: '100%',
+        // width: '100%',
         maxWidth: 400,
-        margin: '10px 20px 40px 20px',
+        margin: '10px 0px 40px 0px',
         padding: '10px 10px 10px 10px',
         borderRadius: 5,
         border: 'none',
@@ -123,13 +130,16 @@ const styles = ({
     },
     wrapper: {
         // backgroundColor: 'blue',
+        display: 'flex',
+        flexDirection: 'column',
         padding: 20,
     },
     branchContainer: {
         borderRadius: 10,
         // backgroundColor: 'red',
         height: 200,
-        margin: '20px 0 20px 0'
+        margin: '20px 0 20px 0',
+        position: 'relative',
     },
     branchTitle: {
         fontFamily: 'raleway-bold',
@@ -157,8 +167,44 @@ const styles = ({
         justifyContent: 'center',
         border: '2px solid black',
         padding: '0 10px 0 10px',
-        margin: '0 0 0 20px',
+        margin: '0 0 60px 0px',
     },
+    image1: {
+        position: 'absolute',
+        left: 40,
+        right: 0,
+        top: 30,
+        bottom: 0,
+        backgroundImage: `url(${branch1})`,
+        backgroundSize: 'cover',
+        borderRadius: 10,
+        zIndex: -1,
+        opacity: 0.2,
+    },
+    image2: {
+        position: 'absolute',
+        left: 40,
+        right: 0,
+        top: 30,
+        bottom: 0,
+        backgroundImage: `url(${branch2})`,
+        backgroundSize: 'cover',
+        borderRadius: 10,
+        zIndex: -1,
+        opacity: 0.2,
+    },
+    image3: {
+        position: 'absolute',
+        left: 40,
+        right: 0,
+        top: 30,
+        bottom: 0,
+        backgroundImage: `url(${branch3})`,
+        backgroundSize: 'cover',
+        borderRadius: 10,
+        zIndex: -1,
+        opacity: 0.2,
+    }
 });
 
 export default Contact;
